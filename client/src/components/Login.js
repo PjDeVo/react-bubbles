d92b8b4cdf3d6c3e0c79fe6aa4fa823115869302
Owner <owner@Users-MacBook-Pro.local>
2020-05-03 21:38:29 -0400

    private route created. Token retrived from API using axiosWithAuth call and set to localStorage PrivateRoute created for bubbles page and dested using delete button

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utilities/axiosWithAuth";
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+  const handleChanges = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/login", user)
+      .then((res) => {
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/bubbles");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    // console.log(user);
+  };
+
+  const logOut = () => {
+    localStorage.removeItem("token");
+  };
+
   return (
-    <>
+    <div>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+      <button onClick={logOut}> Logout</button>
+
+      <form onSubmit={onSubmit} className="login-form">
+        <label>Username</label>
+        <input
+          type="text"
+          value={user.username}
+          placeholer="Username"
+          onChange={handleChanges}
+          name="username"
+        />
+        <label> Password</label>
+        <input
+          type="text"
+          value={user.password}
+          placeholer="Password"
+          onChange={handleChanges}
+          name="password"
+        />
+        <button> Login</button>
+      </form>
+    </div>
   );
 };
 
